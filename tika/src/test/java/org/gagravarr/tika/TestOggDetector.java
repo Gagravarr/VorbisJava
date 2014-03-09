4876b4d8d9d8a46afe149695ab9b25b35f82396b
Nick Burch <nick@gagravarr.org>
2014-03-09 10:43:53 +0000

    Fix inconsistent whitespace/indent in the Tika Detect test

--- tika/src/test/java/org/gagravarr/tika/TestOggDetector.java
+++ tika/src/test/java/org/gagravarr/tika/TestOggDetector.java
@@ -25,62 +25,62 @@ import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
 
 public class TestOggDetector extends TestCase {
-   protected InputStream getTestOggFile() throws IOException {
-      return this.getClass().getResourceAsStream("/testBoundaries.ogg");
-   }
-	protected InputStream getTestVorbisFile() throws IOException {
-		return this.getClass().getResourceAsStream("/testVORBIS.ogg");
-	}
-	protected InputStream getTestFlacOggFile() throws IOException {
-      return this.getClass().getResourceAsStream("/testFLAC.oga");
-   }
-	protected InputStream getTestFlacNativeFile() throws IOException {
-      return this.getClass().getResourceAsStream("/testFLAC.flac");
-   }
-	protected InputStream getDummy() throws IOException {
-	   return new ByteArrayInputStream(new byte[] { 0,1,2,3,4,5,6,7 });
-	}
-	
-	public void testDetect() throws IOException {
-	   OggDetector d = new OggDetector();
-	   Metadata m = new Metadata();
-	   
-	   // Needs to be a TikaInputStream to be detected properly
-      assertEquals(
-            OggDetector.OGG_GENERAL,
-            d.detect(new BufferedInputStream(getTestOggFile()), m)
-      );
-      assertEquals(
-            OggDetector.OGG_VORBIS, 
-            d.detect(TikaInputStream.get(getTestVorbisFile()), m)
-      );
-	   
-	   // Various Ogg based formats
-      assertEquals(
-            OggDetector.OGG_VORBIS, 
-            d.detect(TikaInputStream.get(getTestVorbisFile()), m)
-      );
-      assertEquals(
-            OggDetector.FLAC, 
-            d.detect(TikaInputStream.get(getTestFlacOggFile()), m)
-      );
-      assertEquals(
-            OggDetector.OGG_GENERAL, 
-            d.detect(TikaInputStream.get(getTestOggFile()), m)
-      );
-      
-      // TODO Video
-      
-      // It won't detect native FLAC files however
-      assertEquals(
-            MediaType.OCTET_STREAM, 
-            d.detect(TikaInputStream.get(getTestFlacNativeFile()), m)
-      );
-      
-      // Random junk is a Octet Stream too
-      assertEquals(
-            MediaType.OCTET_STREAM, 
-            d.detect(TikaInputStream.get(getDummy()), m)
-      );
-	}
+    protected InputStream getTestOggFile() throws IOException {
+        return this.getClass().getResourceAsStream("/testBoundaries.ogg");
+    }
+    protected InputStream getTestVorbisFile() throws IOException {
+        return this.getClass().getResourceAsStream("/testVORBIS.ogg");
+    }
+    protected InputStream getTestFlacOggFile() throws IOException {
+        return this.getClass().getResourceAsStream("/testFLAC.oga");
+    }
+    protected InputStream getTestFlacNativeFile() throws IOException {
+        return this.getClass().getResourceAsStream("/testFLAC.flac");
+    }
+    protected InputStream getDummy() throws IOException {
+        return new ByteArrayInputStream(new byte[] { 0,1,2,3,4,5,6,7 });
+    }
+
+    public void testDetect() throws IOException {
+        OggDetector d = new OggDetector();
+        Metadata m = new Metadata();
+
+        // Needs to be a TikaInputStream to be detected properly
+        assertEquals(
+                OggDetector.OGG_GENERAL,
+                d.detect(new BufferedInputStream(getTestOggFile()), m)
+        );
+        assertEquals(
+                OggDetector.OGG_VORBIS, 
+                d.detect(TikaInputStream.get(getTestVorbisFile()), m)
+        );
+
+        // Various Ogg based formats
+        assertEquals(
+                OggDetector.OGG_VORBIS, 
+                d.detect(TikaInputStream.get(getTestVorbisFile()), m)
+        );
+        assertEquals(
+                OggDetector.FLAC, 
+                d.detect(TikaInputStream.get(getTestFlacOggFile()), m)
+        );
+        assertEquals(
+                OggDetector.OGG_GENERAL, 
+                d.detect(TikaInputStream.get(getTestOggFile()), m)
+        );
+
+        // TODO Video
+
+        // It won't detect native FLAC files however
+        assertEquals(
+                MediaType.OCTET_STREAM, 
+                d.detect(TikaInputStream.get(getTestFlacNativeFile()), m)
+        );
+
+        // Random junk is a Octet Stream too
+        assertEquals(
+                MediaType.OCTET_STREAM, 
+                d.detect(TikaInputStream.get(getDummy()), m)
+        );
+    }
 }
