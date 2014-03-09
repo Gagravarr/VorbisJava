1c07efaf25d11b2110658c5ee254bf69bc1b05d5
Nick Burch <nick@gagravarr.org>
2014-03-09 10:32:19 +0000

    Fix inconsistent whitespace/indent in the Tika Vorbis test

--- tika/src/test/java/org/gagravarr/tika/TestVorbisParser.java
+++ tika/src/test/java/org/gagravarr/tika/TestVorbisParser.java
@@ -28,50 +28,51 @@ import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.ContentHandler;
 
 public class TestVorbisParser extends TestCase {
-	private InputStream getTestFile() throws IOException {
-		return this.getClass().getResourceAsStream("/testVORBIS.ogg");
-	}
-	
+    private InputStream getTestFile() throws IOException {
+        return this.getClass().getResourceAsStream("/testVORBIS.ogg");
+    }
+
+    @SuppressWarnings("deprecation")
     public void testParser() throws Exception {
-	   VorbisParser parser = new VorbisParser();
-	   
-      ContentHandler handler = new BodyContentHandler();
-      ParseContext context = new ParseContext();
-	   Metadata metadata = new Metadata();
-	   
-	   parser.parse(
-	         TikaInputStream.get(getTestFile()), handler,
-	         metadata, context
-	   );
-	   
-	   // Check legacy style metadata
-	   assertEquals("Test Artist", metadata.get(Metadata.AUTHOR));
-      assertEquals("Test Title", metadata.get(Metadata.TITLE));
-      
-      // Check new-style metadata
-      assertEquals("Test Artist", metadata.get(Office.AUTHOR));
-      assertEquals("Test Artist", metadata.get(TikaCoreProperties.CREATOR));
-      assertEquals("Test Title", metadata.get(TikaCoreProperties.TITLE));
-      
-      assertEquals("Test Album", metadata.get(XMPDM.ALBUM));
-      assertEquals("Test Artist", metadata.get(XMPDM.ARTIST));
-      assertEquals("Stereo", metadata.get(XMPDM.AUDIO_CHANNEL_TYPE));
-      assertEquals("44100", metadata.get(XMPDM.AUDIO_SAMPLE_RATE));
-      assertEquals("Test Genre", metadata.get(XMPDM.GENRE));
-      assertEquals("Test Comment", metadata.get(XMPDM.LOG_COMMENT));
-      assertEquals("2010-01-26", metadata.get(XMPDM.RELEASE_DATE));
-      assertEquals("1", metadata.get(XMPDM.TRACK_NUMBER));
-      
-      assertEquals("Xiph.Org libVorbis I 20070622", metadata.get("vendor"));
-      assertEquals("Vorbis 0", metadata.get("version"));
-      
-      // Check text
-      String content = handler.toString();
-      assertTrue(content.contains("Test Title"));
-      assertTrue(content.contains("Test Artist"));
-      assertTrue(content.contains("Test Album"));
-      assertTrue(content.contains("2010"));
-      assertTrue(content.contains("Test Comment"));
-      assertTrue(content.contains("Test Genre"));
-	}
+        VorbisParser parser = new VorbisParser();
+
+        ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
+        Metadata metadata = new Metadata();
+
+        parser.parse(
+                TikaInputStream.get(getTestFile()), handler,
+                metadata, context
+        );
+
+        // Check legacy style metadata
+        assertEquals("Test Artist", metadata.get(Metadata.AUTHOR));
+        assertEquals("Test Title", metadata.get(Metadata.TITLE));
+
+        // Check new-style metadata
+        assertEquals("Test Artist", metadata.get(Office.AUTHOR));
+        assertEquals("Test Artist", metadata.get(TikaCoreProperties.CREATOR));
+        assertEquals("Test Title", metadata.get(TikaCoreProperties.TITLE));
+
+        assertEquals("Test Album", metadata.get(XMPDM.ALBUM));
+        assertEquals("Test Artist", metadata.get(XMPDM.ARTIST));
+        assertEquals("Stereo", metadata.get(XMPDM.AUDIO_CHANNEL_TYPE));
+        assertEquals("44100", metadata.get(XMPDM.AUDIO_SAMPLE_RATE));
+        assertEquals("Test Genre", metadata.get(XMPDM.GENRE));
+        assertEquals("Test Comment", metadata.get(XMPDM.LOG_COMMENT));
+        assertEquals("2010-01-26", metadata.get(XMPDM.RELEASE_DATE));
+        assertEquals("1", metadata.get(XMPDM.TRACK_NUMBER));
+
+        assertEquals("Xiph.Org libVorbis I 20070622", metadata.get("vendor"));
+        assertEquals("Vorbis 0", metadata.get("version"));
+
+        // Check text
+        String content = handler.toString();
+        assertTrue(content.contains("Test Title"));
+        assertTrue(content.contains("Test Artist"));
+        assertTrue(content.contains("Test Album"));
+        assertTrue(content.contains("2010"));
+        assertTrue(content.contains("Test Comment"));
+        assertTrue(content.contains("Test Genre"));
+    }
 }
