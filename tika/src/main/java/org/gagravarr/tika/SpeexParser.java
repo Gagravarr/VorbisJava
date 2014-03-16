fb5a60ca65f3ef5d8421a4bec1d94f2d4ff05002
Nick Burch <nick@gagravarr.org>
2014-03-16 09:33:53 +0000

    Speex Tika parser

---
+++ tika/src/main/java/org/gagravarr/tika/SpeexParser.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gagravarr.tika;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.XMPDM;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.XHTMLContentHandler;
+import org.gagravarr.ogg.OggFile;
+import org.gagravarr.speex.SpeexFile;
+import org.gagravarr.speex.SpeexInfo;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+/**
+ * Parser for OGG Speex audio files
+ */
+public class SpeexParser extends OggAudioParser {
+   private static final long serialVersionUID = 5904981674814527529L;
+
+   private static List<MediaType> TYPES = Arrays.asList(new MediaType[] {
+         OggDetector.OPUS_AUDIO, OggDetector.OPUS_AUDIO_ALT
+   });
+   
+   public Set<MediaType> getSupportedTypes(ParseContext context) {
+      return new HashSet<MediaType>(TYPES);
+   }
+   
+   public void parse(
+         InputStream stream, ContentHandler handler,
+         Metadata metadata, ParseContext context)
+         throws IOException, TikaException, SAXException {
+      metadata.set(Metadata.CONTENT_TYPE, OggDetector.OPUS_AUDIO.toString());
+      metadata.set(XMPDM.AUDIO_COMPRESSOR, "Speex");
+
+      // Open the process the files
+      OggFile ogg = new OggFile(stream);
+      SpeexFile speex = new SpeexFile(ogg);
+
+      // Start
+      XHTMLContentHandler xhtml = new XHTMLContentHandler(handler, metadata);
+      xhtml.startDocument();
+
+      // Extract the common Speex info
+      extractInfo(metadata, speex.getInfo());
+      
+      // Extract any Vorbis comments
+      extractComments(metadata, xhtml, speex.getTags());
+      
+      // TODO Work out the song length, and return that
+
+      // Finish
+      xhtml.endDocument();
+   }
+   
+   protected void extractInfo(Metadata metadata, SpeexInfo info) throws TikaException {
+      metadata.set(XMPDM.AUDIO_SAMPLE_RATE, (int)info.getRate());
+      metadata.add("version", "Speex " + info.getVersionId() + " - " + info.getVersionString());
+    
+      extractChannelInfo(metadata, info.getChannels());
+   }
+}
---
+++ tika/src/test/java/org/gagravarr/tika/TestSpeexParser.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gagravarr.tika;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import junit.framework.TestCase;
+
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.Office;
+import org.apache.tika.metadata.TikaCoreProperties;
+import org.apache.tika.metadata.XMPDM;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
+
+public class TestSpeexParser extends TestCase {
+    private InputStream getTestFile() throws IOException {
+        return this.getClass().getResourceAsStream("/testSPEEX.spx");
+    }
+
+    @SuppressWarnings("deprecation")
+    public void testParser() throws Exception {
+        SpeexParser parser = new SpeexParser();
+
+        ContentHandler handler = new BodyContentHandler();
+        ParseContext context = new ParseContext();
+        Metadata metadata = new Metadata();
+
+        parser.parse(
+                TikaInputStream.get(getTestFile()), handler,
+                metadata, context
+        );
+
+        // Check legacy style metadata
+        assertEquals("Test Artist", metadata.get(Metadata.AUTHOR));
+        assertEquals("Test Title", metadata.get(Metadata.TITLE));
+
+        // Check new-style metadata
+        assertEquals("Test Artist", metadata.get(Office.AUTHOR));
+        assertEquals("Test Artist", metadata.get(TikaCoreProperties.CREATOR));
+        assertEquals("Test Title", metadata.get(TikaCoreProperties.TITLE));
+
+        assertEquals("Test Album", metadata.get(XMPDM.ALBUM));
+        assertEquals("Test Artist", metadata.get(XMPDM.ARTIST));
+        assertEquals("Stereo", metadata.get(XMPDM.AUDIO_CHANNEL_TYPE));
+        assertEquals("44100", metadata.get(XMPDM.AUDIO_SAMPLE_RATE));
+        assertEquals("Test Genre", metadata.get(XMPDM.GENRE));
+        assertEquals("Test Comment", metadata.get(XMPDM.LOG_COMMENT));
+        assertEquals("2010-01-26", metadata.get(XMPDM.RELEASE_DATE));
+        assertEquals("1", metadata.get(XMPDM.TRACK_NUMBER));
+
+        assertEquals("Encoded with Speex 1.2rc1", metadata.get("vendor"));
+        assertEquals("Speex 1 - 1.2rc1", metadata.get("version"));
+
+        // Check text
+        String content = handler.toString();
+        assertTrue(content.contains("Test Title"));
+        assertTrue(content.contains("Test Artist"));
+        assertTrue(content.contains("Test Album"));
+        assertTrue(content.contains("2010"));
+        assertTrue(content.contains("Test Comment"));
+        assertTrue(content.contains("Test Genre"));
+    }
+}
