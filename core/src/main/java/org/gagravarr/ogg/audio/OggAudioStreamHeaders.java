d832b14271ce7e14b4b6becd530c833cdee0118c
Nick Burch <nick@gagravarr.org>
2014-09-03 21:42:14 +0100

    More generic audio packet creation

--- core/src/main/java/org/gagravarr/ogg/audio/OggAudioStreamHeaders.java
+++ core/src/main/java/org/gagravarr/ogg/audio/OggAudioStreamHeaders.java
@@ -16,19 +16,16 @@ package org.gagravarr.ogg.audio;
 import org.gagravarr.flac.FlacFirstOggPacket;
 import org.gagravarr.flac.FlacTags;
 import org.gagravarr.ogg.OggPacket;
+import org.gagravarr.ogg.OggStreamAudioData;
 import org.gagravarr.ogg.OggStreamIdentifier;
 import org.gagravarr.ogg.OggStreamIdentifier.OggStreamType;
+import org.gagravarr.ogg.OggStreamPacket;
 import org.gagravarr.opus.OpusInfo;
 import org.gagravarr.opus.OpusPacketFactory;
-import org.gagravarr.opus.OpusTags;
 import org.gagravarr.speex.SpeexInfo;
 import org.gagravarr.speex.SpeexPacketFactory;
-import org.gagravarr.speex.SpeexTags;
-import org.gagravarr.vorbis.VorbisComments;
 import org.gagravarr.vorbis.VorbisInfo;
-import org.gagravarr.vorbis.VorbisPacket;
 import org.gagravarr.vorbis.VorbisPacketFactory;
-import org.gagravarr.vorbis.VorbisSetup;
 
 
 /**
@@ -86,27 +83,31 @@ public class OggAudioStreamHeaders implements OggAudioHeaders {
     }
 
     /**
+     * Creates an appropriate high level packet
+     */
+    protected OggStreamPacket createNext(OggPacket packet) {
+        if (type == OggStreamIdentifier.OGG_VORBIS) {
+            return VorbisPacketFactory.create(packet);
+        } else if (type == OggStreamIdentifier.SPEEX_AUDIO) {
+            return SpeexPacketFactory.create(packet);
+        } else if (type == OggStreamIdentifier.OPUS_AUDIO) {
+            return OpusPacketFactory.create(packet);
+        } else if (type == OggStreamIdentifier.OGG_FLAC) {
+            // TODO Finish FLAC support
+            return null;
+        } else {
+            throw new IllegalArgumentException("Unsupported stream of type " + type);
+        }
+    }
+
+    /**
      * Populates with the next header
      *
      * @return Do any more headers remain to be populated?
      */
     public boolean populate(OggPacket packet) {
-        if (type == OggStreamIdentifier.OGG_VORBIS) {
-            VorbisPacket pkt = VorbisPacketFactory.create(packet);
-            if (pkt instanceof VorbisComments) {
-                tags = (VorbisComments)pkt;
-                return true;
-            } else {
-                setup = (VorbisSetup)pkt;
-                return false;
-            }
-        } else if (type == OggStreamIdentifier.SPEEX_AUDIO) {
-            tags = (SpeexTags)SpeexPacketFactory.create(packet);
-            return false;
-        } else if (type == OggStreamIdentifier.OPUS_AUDIO) {
-            tags = (OpusTags)OpusPacketFactory.create(packet);
-            return false;
-        } else if (type == OggStreamIdentifier.OGG_FLAC) {
+        // TODO Finish the flac support properly
+        if (type == OggStreamIdentifier.OGG_FLAC) {
             if (tags == null) {
                 tags = new FlacTags(packet);
                 return true;
@@ -114,9 +115,34 @@ public class OggAudioStreamHeaders implements OggAudioHeaders {
                 // TODO Finish FLAC support
                 return false;
             }
-        } else {
-            throw new IllegalArgumentException("Unsupported stream of type " + type);
         }
+
+        OggStreamPacket sPacket = createNext(packet);
+        if (sPacket instanceof OggAudioTagsHeader) {
+            tags = (OggAudioTagsHeader)sPacket;
+
+            // Are there more headers to come?
+            if (type == OggStreamIdentifier.OGG_VORBIS) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (sPacket instanceof OggAudioSetupHeader) {
+            setup = (OggAudioSetupHeader)sPacket;
+
+            // Setup is always last
+            return false;
+        }
+
+        throw new IllegalArgumentException("Expecting header packet but got " + sPacket);
+    }
+
+    /**
+     * Creates the Audio Data for a given audio packet
+     */
+    public OggStreamAudioData createAudio(OggPacket packet) {
+        return (OggStreamAudioData)createNext(packet);
     }
 
 
