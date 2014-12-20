c74f9d4680ee205f0d2f6c4c03d6a0a3e7fb8f60
Andreas Mack <andreas.mack@konsec.com>
2014-12-20 16:02:49 +0100

    Formatting

--- tools/src/main/java/org/gagravarr/opus/tools/OpusCommentTool.java
+++ tools/src/main/java/org/gagravarr/opus/tools/OpusCommentTool.java
@@ -29,68 +29,70 @@ import org.gagravarr.vorbis.tools.VorbisLikeCommentTool.Command.Commands;
  *  Opus File, much like the vorbiscomments program (but Opus)
  */
 public class OpusCommentTool extends VorbisLikeCommentTool {
-	
+
 	private static String format1(double d) {
 		return String.format("%.2f",d);
 	}
 	private static String format2(double d) {
 		return String.format("%8.1f",d);
 	}
-    public static void main(String[] args) throws Exception {
-        Command command = processArgs(args, "OpusComment");
-        
-        OpusFile op = new OpusFile(new File(command.inFile));
-        
-        if (command.command == Commands.List) {
-            listTags(op.getTags());
-            OpusInfo info = op.getInfo();
-            
-            System.out.println("\tPre-skip: " + info.getPreSkip());
-            System.out.println("\tPlayback gain: "+info.getOutputGain()+" dB");
-            System.out.println("\tChannels: "+info.getChannels());
-            System.out.println("\tOriginal sample rate: "+ info.getRate()+"Hz");
-            System.out.println("\tPacket duration: "+ format2(info.getMaxPacketDuration())+"ms (max), "
-                    +format2(info.getAvgPacketDuration())+"ms (avg), "
-            		+format2(info.getMinPacketDuration())+"ms (min)");
-            System.out.println("\tPage duration:   "+ format2(info.getMaxPageDuration())+"ms (max), "
-            		+format2(info.getAvgPageDuration())+"ms (avg), "          		
-            		+format2(info.getMinPageDuration())+"ms (min)");
-            //DecimalFormat df2 = new DecimalFormat("#.00");
-            System.out.println("\tTotal data length: "+ info.getBytes()+" (overhead: "
-            		+format1(((double)info.getOverheadBytes()/info.getBytes()*100.0))+"%)");
-            System.out.println("\tPlayback length: "+ info.getPlayTimeAsString());
-            double avgbr = (info.getBytes()*8) / info.getPlayTime();
-            double avgbrover = ((info.getBytes()-info.getOverheadBytes())*8) / info.getPlayTime();
-            String cbr = "";
-            if (info.getMinPacketDuration() == info.getMaxPacketDuration()
-            		&& info.getMinPacketBytes() == info.getMaxPacketBytes()) {
-            	cbr= " (hard-CBR)";
-            }
-            System.out.println("\tAverage bitrate: "+ format1(avgbr) + " kb/s, w/o overhead: "+format1(avgbrover)+" kb/s"+cbr);
-            
-        } else {
-            // Have the new tags added
-            addTags(op.getTags(), command);
-            
-            // Write out
-            List<OpusAudioData> audio = new ArrayList<OpusAudioData>();
-            OpusAudioData ad;
-            while( (ad = op.getNextAudioPacket()) != null ) {
-                audio.add(ad);
-            }
+	public static void main(String[] args) throws Exception {
+		Command command = processArgs(args, "OpusComment");
 
-            // Now write out
-            op.close();
-            OpusFile out = new OpusFile(
-                    new FileOutputStream(command.outFile),
-                    op.getSid(),
-                    op.getInfo(),
-                    op.getTags()
-            );
-            for(OpusAudioData oad : audio) {
-                out.writeAudioData(oad);
-            }
-            out.close();
-        }
-    }
+		OpusFile op = new OpusFile(new File(command.inFile));
+
+		if (command.command == Commands.List) {
+			listTags(op.getTags());
+			OpusInfo info = op.getInfo();
+
+			System.out.println("\tPre-skip: " + info.getPreSkip());
+			System.out.println("\tPlayback gain: "+info.getOutputGain()+" dB");
+			System.out.println("\tChannels: "+info.getChannels());
+			System.out.println("\tOriginal sample rate: "+ info.getRate()+"Hz");
+			if (info.getBytes()> 0) {
+				System.out.println("\tPacket duration: "+ format2(info.getMaxPacketDuration())+"ms (max), "
+						+format2(info.getAvgPacketDuration())+"ms (avg), "
+						+format2(info.getMinPacketDuration())+"ms (min)");
+				System.out.println("\tPage duration:   "+ format2(info.getMaxPageDuration())+"ms (max), "
+						+format2(info.getAvgPageDuration())+"ms (avg), "          		
+						+format2(info.getMinPageDuration())+"ms (min)");
+				//DecimalFormat df2 = new DecimalFormat("#.00");
+
+				System.out.println("\tTotal data length: "+ info.getBytes()+" (overhead: "
+						+format1(((double)info.getOverheadBytes()/info.getBytes()*100.0))+"%)");
+				System.out.println("\tPlayback length: "+ info.getPlayTimeAsString());
+				double avgbr = (info.getBytes()*8) / info.getPlayTime();
+				double avgbrover = ((info.getBytes()-info.getOverheadBytes())*8) / info.getPlayTime();
+				String cbr = "";
+				if (info.getMinPacketDuration() == info.getMaxPacketDuration()
+						&& info.getMinPacketBytes() == info.getMaxPacketBytes()) {
+					cbr= " (hard-CBR)";
+				}
+				System.out.println("\tAverage bitrate: "+ format1(avgbr) + " kb/s, w/o overhead: "+format1(avgbrover)+" kb/s"+cbr);
+			}
+		} else {
+			// Have the new tags added
+			addTags(op.getTags(), command);
+
+			// Write out
+			List<OpusAudioData> audio = new ArrayList<OpusAudioData>();
+			OpusAudioData ad;
+			while( (ad = op.getNextAudioPacket()) != null ) {
+				audio.add(ad);
+			}
+
+			// Now write out
+			op.close();
+			OpusFile out = new OpusFile(
+					new FileOutputStream(command.outFile),
+					op.getSid(),
+					op.getInfo(),
+					op.getTags()
+					);
+			for(OpusAudioData oad : audio) {
+				out.writeAudioData(oad);
+			}
+			out.close();
+		}
+	}
 }
