422b6e0a974273577993dfaddc611f1bd9ecc71e
Nick Burch <nick@gagravarr.org>
2014-12-24 06:12:42 +0000

    Start to refactor out the audio statistics gathering

---
+++ core/src/main/java/org/gagravarr/ogg/audio/OggAudioStatistics.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gagravarr.ogg.audio;
+
+import java.io.IOException;
+
+import org.gagravarr.ogg.OggStreamAudioData;
+
+
+/**
+ * For computing statistics around an {@link OggAudioStream},
+ *  such as how long it lasts.
+ * Format specific subclasses may be able to also identify 
+ *  additional statistics beyond these.
+ */
+public class OggAudioStatistics {
+    private int dataPackets = 0;
+    private long dataSize = 0;
+    private long lastGranule = -1;
+    private int durationSeconds = 0;
+
+    public OggAudioStatistics(OggAudioStream audio, long sampleRate) throws IOException {
+        OggStreamAudioData data;
+
+        // Have each audio packet handled, tracking at least granules
+        while ((data = audio.getNextAudioPacket()) != null) {
+            handleAudioData(data);
+        }
+
+        // Calculate the duration from the granules, if found
+        if (lastGranule > 0) {
+            durationSeconds = (int)(lastGranule/sampleRate);
+        }
+    }
+
+    protected void handleAudioData(OggStreamAudioData audioData) {
+        dataPackets++;
+        dataSize += audioData.getData().length;
+
+        if (audioData.getGranulePosition() > lastGranule) {
+            lastGranule = audioData.getGranulePosition();
+        }
+    }
+
+    /**
+     * Returns the duration of the audio, in seconds.
+     */
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+    /**
+     * The last granule (time position) in the audio stream
+     */
+    public long getLastGranule() {
+        return lastGranule;
+    }
+
+    /**
+     * The number of audio packets in the stream
+     */
+    public int getDataPackets() {
+        return dataPackets;
+    }
+    /**
+     * The size, in bytes, of all the audio data
+     */
+    public long getDataSize() {
+        return dataSize;
+    }
+}
