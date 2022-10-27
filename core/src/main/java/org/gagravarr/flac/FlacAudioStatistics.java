bfe0b46d4cabb5ec3ae1fed856721b00336c47db
Nick Burch <nick@gagravarr.org>
2022-10-27 21:40:55 +0100

    Calculate the FLAC audio duration from frames, eg for streamed or partial files

---
+++ core/src/main/java/org/gagravarr/flac/FlacAudioStatistics.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gagravarr.flac;
+
+import java.io.File;
+import java.io.IOException;
+
+/** 
+ * For computing statistics around a {@link FlacFile},
+ *  such as how long it lasts.
+ * Most encoders will include this information in the
+ *  {@link FlacInfo} header.
+ */
+public class FlacAudioStatistics {
+   private final FlacFile flac;
+
+   private int audioFrames = 0;
+   private long wastedBits = 0;
+   private double durationSeconds = 0;
+
+   public FlacAudioStatistics(FlacFile flac) {
+      this.flac = flac;
+   }
+
+   /**
+    * Calculate the statistics
+    */
+   public void calculate() throws IOException {
+      FlacAudioFrame data;
+
+      while ((data = flac.getNextAudioPacket()) != null) {
+         handleAudioData(data);
+      }
+   }
+
+   protected void handleAudioData(FlacAudioFrame audio) {
+      audioFrames++;
+      for (FlacAudioSubFrame s : audio.getSubFrames()) {
+         wastedBits += s.getWastedBits();
+      }
+      // TODO Is this correct?
+      double frameDuration = (double)audio.getBlockSize() / audio.getSampleRate();
+      durationSeconds += frameDuration;
+      // TODO Should we calculate any overheads?
+   }
+
+   /**
+    * Returns the duration of the audio, in seconds.
+    */
+   public double getDurationSeconds() {
+       return durationSeconds;
+   }
+
+   /**
+    * The number of audio frames in the stream
+    */
+   public int getAudioFramesCount() {
+       return audioFrames;
+   }
+
+   /**
+    * The total number of wasted bits, across all
+    *  the audio subframes in the stream
+    */
+   public long getWastedBits() {
+       return wastedBits;
+   }
+}
